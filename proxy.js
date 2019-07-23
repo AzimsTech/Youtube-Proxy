6c26d590f4c37d28c2765b5427719a61a4fd2cad
Wan Muhammad Azims <4zims95@gmail.com>
2019-07-24 02:48:15 +0800

    Initial commit

--- proxy.js
+++ proxy.js
@@ -2,9 +2,13 @@
   var config = {
     mode: "pac_script",
     pacScript: {
+      // use proxy for specific domains
       data: "function FindProxyForURL(url, host) {\n" +
             "  if (shExpMatch(host, '*.googlevideo.com|*.youtube.com'))\n" +
             "    return 'PROXY 47.254.202.128:3128';\n" +
+
+            
+            // by default use no proxy
             "  return 'DIRECT';\n" +
             "}"
     }
